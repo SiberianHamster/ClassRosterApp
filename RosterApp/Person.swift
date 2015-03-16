7ae38d4b1e778b209fd4385c474ec472964568ea
Mark Lin <m4rklin@comcast.net>
2015-03-15 22:28:54 -0700

    Homework8

--- RosterApp/Person.swift
+++ RosterApp/Person.swift
@@ -8,15 +8,29 @@
 
 import UIKit
 
-class Person {
+class Person : NSObject, NSCoding {
   var firstName = "John"
   var lastName = "Doe"
+  var bioOfPerson = "This is some filler text"
   var pictureOfPerson = UIImage (named: "NoImages.png")
-  var bioOFPerson = "This is some filler text"
   
   init (firstName: String, lastName: String){
     self.firstName = firstName
     self.lastName = lastName
+  }
+  
+  required init(coder aDecoder: NSCoder) {
+    self.firstName = aDecoder.decodeObjectForKey("firstName") as String
+    self.lastName = aDecoder.decodeObjectForKey("lastName") as String
+    self.pictureOfPerson = aDecoder.decodeObjectForKey("image") as? UIImage
+    self.bioOfPerson = aDecoder.decodeObjectForKey("bioText") as String
+  }
+  
+  func encodeWithCoder(aCoder: NSCoder) {
+    aCoder.encodeObject(self.firstName, forKey: "firstName")
+    aCoder.encodeObject(self.lastName, forKey: "lastName")
+    aCoder.encodeObject(self.pictureOfPerson, forKey: "image")
+    aCoder.encodeObject(self.bioOfPerson, forKey: "bioText")
   }
 }
 
--- RosterApp/ViewController.swift
+++ RosterApp/ViewController.swift
@@ -14,87 +14,48 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
   
   func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
     return self.names.count
-    
   }
   
   var names = [Person]()
   
   override func viewDidLoad() {
-    self.tableView.dataSource = self
-    self.tableView.delegate = self
     super.viewDidLoad()
-    
-    if let filePath = NSBundle.mainBundle().pathForResource("People", ofType: "plist"){
-      println(filePath)
-      if let plistArray = NSArray(contentsOfFile: filePath){
-        println(plistArray.count)
-      }
-    }
-    
 
+    self.loadFromArchive()
     
-//    let Amethyst = Person(firstName: "Amy", lastName: "Winston")
-//    let Aquaman = Person(firstName: "Arthur", lastName: "Curry")
-//    let Batgirl = Person(firstName: "Barbara", lastName: "Gordon")
-//    let Batman = Person(firstName: "Bruce", lastName: "Wayne")
-//    let Beastboy = Person(firstName: "Garfield", lastName: "Logan")
-//    let Catwoman = Person(firstName: "Selina", lastName: "Kyle")
-//    let Cyborg = Person(firstName: "Victor", lastName: "Stone")
-//    let Flash = Person(firstName: "Barry", lastName: "Allen")
-//    let Greenarrow = Person(firstName: "Oliver", lastName: "Queen")
-//    let Greenlantern = Person(firstName: "Hal", lastName: "Jordan")
-//    let Hawkgirl = Person(firstName: "Shayera", lastName: "Hol")
-//    let Hawkman = Person(firstName: "Carter", lastName: "Hall")
-//    let Mera = Person(firstName: "Mera", lastName: "")
-//    let Nightwing = Person(firstName: "Richard", lastName: "Grayson")
-//    let Pandora = Person(firstName: "Pandora", lastName: "")
-//    let Powergirl = Person(firstName: "Kara", lastName: "Zor-El")
-//    let Raven = Person(firstName: "Rachel", lastName: "Roth")
-//    let Robin = Person(firstName: "Richard", lastName: "Grayson")
-//    let Shazam = Person(firstName: "Billy", lastName: "Batson")
-//    let Superman = Person(firstName: "Kal", lastName: "El")
-//    let Supergirl = Person(firstName: "Kara", lastName: "Zor-El")
-//    let Starfire = Person(firstName: "Koriand'r", lastName: "")
-//    let Wonderwoman = Person(firstName: "Diana", lastName: "")
-//    let Zatanna = Person(firstName: "Zatanna", lastName: "Zatara")
-//    self.names.append(Amethyst)
-//    self.names.append(Aquaman)
-//    self.names.append(Batgirl)
-//    self.names.append(Batman)
-//    self.names.append(Beastboy)
-//    self.names.append(Catwoman)
-//    self.names.append(Cyborg)
-//    self.names.append(Flash)
-//    self.names.append(Greenarrow)
-//    self.names.append(Greenlantern)
-//    self.names.append(Hawkgirl)
-//    self.names.append(Hawkman)
-//    self.names.append(Mera)
-//    self.names.append(Nightwing)
-//    self.names.append(Pandora)
-//    self.names.append(Powergirl)
-//    self.names.append(Raven)
-//    self.names.append(Robin)
-//    self.names.append(Shazam)
-//    self.names.append(Superman)
-//    self.names.append(Supergirl)
-//    self.names.append(Starfire)
-//    self.names.append(Wonderwoman)
-//    self.names.append(Zatanna)
+    if self.names.isEmpty {
+      println("loading from plist")
+      if let filePath = NSBundle.mainBundle().pathForResource("People", ofType: "plist") {
+        if let plistArray = NSArray(contentsOfFile: filePath) {
+          for personObject in plistArray {
+            if let personDictionary = personObject as? NSDictionary {
+              let firstName = personDictionary["firstName"] as String
+              let lastName = personDictionary["lastName"] as String
+              let person = Person(firstName: firstName, lastName: lastName)
+              self.names.append(person)
+            }
+          }
+        } else {
+        }
+      }
+      self.saveToArchive()
+    }
+    self.tableView.dataSource = self
   }
   
   override func viewWillAppear(animated: Bool) {
     super.viewWillAppear(animated)
-    self.tableView.reloadData()    
+    self.tableView.reloadData()
+    self.saveToArchive()
   }
   
   func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
     let cell = tableView.dequeueReusableCellWithIdentifier("Cell", forIndexPath: indexPath) as PersonTableViewCell
     let person = self.names[indexPath.row]
-//    cell.textLabel?.text = person.firstName
     cell.FirstNameLabel.text = person.firstName
     cell.LastNameLabel.text = person.lastName
     cell.PictureImage.image = person.pictureOfPerson
+    print(person.bioOfPerson)
     return cell
   }
   
@@ -106,6 +67,31 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
       segueNorm.selectedUser = person
     }
   }
+  
+  func loadFromArchive() {
+    println("Begin loading")
+    let path = getDocumentsPath()
+    let arrayFromArchive = NSKeyedUnarchiver.unarchiveObjectWithFile(path + "/MyArchive") as? [Person]
+    if arrayFromArchive != nil {
+      self.names = arrayFromArchive!
+    }
+    println("End loading")
+  }
+  
+  func saveToArchive() {
+    println("Begin Saving")
+    let path = self.getDocumentsPath()
+    NSKeyedArchiver.archiveRootObject(self.names, toFile: path + "/MyArchive")
+    println("End Saving")
+}
 
+  func getDocumentsPath() -> String {
+    println("Begin getting path")
+    let paths = NSSearchPathForDirectoriesInDomains(NSSearchPathDirectory.DocumentDirectory, NSSearchPathDomainMask.UserDomainMask, true)
+    let path = paths.first as String
+    println(path + " End getting path")
+    return path
+
+  }
 }
 
